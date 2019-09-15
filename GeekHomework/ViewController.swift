ccb62d28f53a39bf4ffd070a9809c5e33380a12f
Nick <nickbarkovski@icloud.com>
2019-09-15 08:47:53 +0300

    Create Project

---
+++ GeekHomework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeekHomework
+//
+//  Created by Николя on 15/09/2019.
+//  Copyright © 2019 nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
